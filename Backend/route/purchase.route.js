ec1ff7245d8431b18ecc516cd904ed2129a3e869
KomalTripathi <komal107tripathi@gmail.com>
2025-04-04 10:40:44 +0530

    corrected user auth

--- Backend/route/purchase.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { purchaseBook } from "../controller/purchase.controller.js";
-
-const router = express.Router();
-
-router.post("/", purchaseBook);
-
-export default router;
